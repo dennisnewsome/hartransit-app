beee7b7232091d0f11cb41f34c1098f98bad70ef
dennisnewsome <newsome.dennis@gmail.com>
2026-08-13 02:53:40 -0400

    Delete hartransit_azure directory

--- hartransit_azure/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Azure App Service startup script
-# Set this as your Startup Command in Azure Portal:
-#   bash /home/site/wwwroot/startup.sh
-
-cd /home/site/wwwroot
-pip install -r requirements.txt --quiet
-uvicorn app.backend.main:app --host 0.0.0.0 --port 8000
